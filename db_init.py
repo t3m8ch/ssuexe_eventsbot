504ac45d2f2ea98cd056cf10ceec8de3857acb35
Artem Kudyakov <t3m8ch@yandex.ru>
2023-10-07 11:53:50 +0400

    fix: Add primary key to id in users table

--- db_init.py
+++ db_init.py
@@ -4,7 +4,7 @@ import aiosqlite
 async def init_db(connection: aiosqlite.Connection):
     await connection.execute(
         'CREATE TABLE IF NOT EXISTS users ('
-        'id INT,'
+        'id INT PRIMARY KEY,'
         'full_name TEXT,'
         'user_name TEXT,'
         'role TEXT'
