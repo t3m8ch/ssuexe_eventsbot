09200d13229a65d7e015ade957ad047bbb950075
Artem Kudyakov <t3m8ch@yandex.ru>
2023-10-08 17:22:55 +0400

    refactor: Make fields of tasks is public in broadcaster

--- infra/broadcaster.py
+++ infra/broadcaster.py
@@ -18,32 +18,32 @@ class BroadcasterTask:
 
 class Broadcaster:
     def __init__(self, tasks: list[BroadcasterTask], event_service: EventService, send_event_service: SendEventService):
-        self._scheduled_tasks = tasks
-        self._performed_tasks: list[BroadcasterTask] = []
-        self._completed_tasks: list[BroadcasterTask] = []
-        self._error_tasks: list[BroadcasterTask] = []
+        self.scheduled_tasks = tasks
+        self.performed_tasks: list[BroadcasterTask] = []
+        self.completed_tasks: list[BroadcasterTask] = []
+        self.error_tasks: list[BroadcasterTask] = []
 
         self._event_service = event_service
         self._send_event_service = send_event_service
 
     async def broadcast(self):
-        while self._scheduled_tasks:
-            self._performed_tasks = self._scheduled_tasks[:LIMIT_MESSAGES_IN_SECOND]
-            del self._scheduled_tasks[:LIMIT_MESSAGES_IN_SECOND]
+        while self.scheduled_tasks:
+            self.performed_tasks = self.scheduled_tasks[:LIMIT_MESSAGES_IN_SECOND]
+            del self.scheduled_tasks[:LIMIT_MESSAGES_IN_SECOND]
 
             start = datetime.now()
 
-            for task in self._performed_tasks:
+            for task in self.performed_tasks:
                 event = await self._event_service.get_event_by_id(task.event_id)
                 try:
                     await self._send_event_service.send_event(event, task.chat_id)
                 except Exception as e:
                     task.error_message = str(e)
-                    self._error_tasks.append(task)
+                    self.error_tasks.append(task)
                 else:
-                    self._completed_tasks.append(task)
+                    self.completed_tasks.append(task)
 
-            self._performed_tasks = []
+            self.performed_tasks = []
 
             end = datetime.now()
 
