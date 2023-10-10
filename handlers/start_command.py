5658d0a4cb11845e7de7388e0e7ddfdacf9f0c6f
Artem Kudyakov <t3m8ch@yandex.ru>
2023-10-10 21:28:04 +0400

    refactor: Use 'user' from middleware data in start_cmd handler

--- handlers/start_command.py
+++ handlers/start_command.py
@@ -1,15 +1,14 @@
 from aiogram import Router, types
 from aiogram.filters import CommandStart
 
-from services.user_service import UserService
+from models.user_model import UserModel
 from utils.menu_kb_gen import generate_menu_kb
 
 router = Router()
 
 
 @router.message(CommandStart())
-async def start_cmd(message: types.Message, user_service: UserService) -> None:
-    user = await user_service.get_user_by_id(message.from_user.id)
+async def start_cmd(message: types.Message, user: UserModel) -> None:
     await message.answer(
         text='Hello, world!',
         reply_markup=generate_menu_kb(user.role),
